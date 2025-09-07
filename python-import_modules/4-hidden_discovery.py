bd399ccbd2a1a22b0688f88c27920a3478f4c39b
Luidgiwtsn <lwbordeaux@gmail.com>
2025-09-07 11:07:14 +0200

    Delete python-import_modules/4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3.10
-if __name__ == "__main__":
-    import hidden_4
-
-    name = dir(hidden_4)
-    for name in sorted(name):
-        if not name.startswith("__"):
-            print(name)

545d053a80cee261d949b5167a5ddf64afde535a
luidgiwtsn <lwbordeaux@gmail.com>
2025-09-15 14:17:40 +0200

    Add 1-main.py|1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class Square whit private attribute size 
+
+"""
+class Square:
+    def __init__(self, size):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the side of the square.
+                        No type or value validation is performed at this stage.
+        """
+        self.__size = size

ece733c354ac93730fd112cf6a4a36b81385889d
luidgiwtsn <lwbordeaux@gmail.com>
2025-09-15 15:58:09 +0200

    Add 3-square.py|3-main.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Module 0-square
-Defines an empty class Square.
+"""
+This module defines a class Square that represents a square and provides
+a method to compute its area. It validates input to ensure only non-negative
+integers can be used as the size of the square.
 """
 
 class Square:
@@ -8,23 +10,27 @@ class Square:
     Represents a square."""
 
 def __init__(self, size=0):
-    """
-    Initializes the square with a given size.
-    Args:
-        size (int): The size of the square (default is 0).
-    Raises:
-        TypeError: If size is not an integer.
-        ValueError: If size is less than 0.
-    """
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = size 
+        """
+        Initializes the square with a given size.
+        
+        Args:
+            size (int): The size of the square (default is 0).
+            
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+       """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size 
 
-def area(self):
-    """ Calculate and return the area of the square.Returns:
+def Area(self):
+    """ 
+    Calculate and return the area of the square.Returns:
+    
     Returns:
         int: The area of the square (size squared).
     """
-    return self.__size * self.__size
+    return self.__size ** 2

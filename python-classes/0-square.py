898a11002841e361885edc5aab6a7c13add4597c
luidgiwtsn <lwbordeaux@gmail.com>
2025-09-15 14:07:43 +0200

    Add 0-square.py|0-main.py

---
+++ python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module 0-square
+Defines an empty class Square.
+"""
+
+class Square:
+    """Represents an empty square.
+
+    This class defines a square without any attributes or methods.
+    It serves as a basic structure for future square-related logic.
+    """
+    pass

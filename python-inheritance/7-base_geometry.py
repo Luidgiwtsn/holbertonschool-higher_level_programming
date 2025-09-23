e589801a293f46227da7acf29786f6a2a686637a
luidgiwtsn <lwbordeaux@gmail.com>
2025-09-23 10:47:48 +0200

    Add 7-base_geometry.py|7-base_geometry.txt

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -20,15 +20,8 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """
         Validates that a value is an integer greater than 0.
-
-        Args:
-            name (str): The name of the value (used in the error message).
-            value (int): The value to validate.
-
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is less than or equal to 0.
         """
+        
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:

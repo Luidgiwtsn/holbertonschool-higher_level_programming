74f538dddca9eb7eb2e02afef644c073a2da0f6f
luidgiwtsn <lwbordeaux@gmail.com>
2025-09-24 13:21:14 +0200

    Add task_00_abc.py| main_00abc.py

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+Defines an abstract Animal class and its subclasses
+dog and cat. Implements the sound method to
+return a specific sound.
+"""
+
+
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    """
+    Abstract base class representing an animal.
+    """
+
+    def sound(self):
+        """
+        Abstract method to be implemented by subclasses.
+        """
+        pass
+
+
+class Dog(Animal):
+    """
+    Represents a dog, subclass of Animal.
+    """
+
+    def sound(self):
+        """
+        Returns the sound made by a dog.
+        """
+        return "Bark"
+
+
+class Cat(Animal):
+    """
+    Represents a cat, subclass of Animal.
+    """
+
+    def sound(self):
+        """
+        Returns the sound made by a cat.
+        """
+        return "Meow"

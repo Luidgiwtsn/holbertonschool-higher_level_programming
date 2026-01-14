7c7461acc9cff0209c03697e0483764783768c1c
luidgiwtsn <lwbordeaux@gmail.com>
2026-01-14 16:01:25 +0100

    Edit 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[39:54] + str[54:55] + str[55:66] + str[66:67] +
-      str[107:111] + str[111:112] + str[0:6])
+str = str[39:66] + str[106:112] + str[0:6]
+print(str)

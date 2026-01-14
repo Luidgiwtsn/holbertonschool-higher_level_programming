08f148354694fc7d2a123d0475b0d8ba6fce3f34
luidgiwtsn <lwbordeaux@gmail.com>
2026-01-14 15:26:28 +0100

    Edit 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Print 'object-oriented programming with Python' using slicing only."""
-str = ("Python is an interpreted, interactive, object-oriented programming"
-"language that combines remarkable power with very clear syntax")
-print(str[38:66] + " with " + str[:6])
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+"""Affiche 'object-oriented programming with Python' via slicing"""
+print(str[39:54] + str[54:55] + str[55:66] + str[66:67] +
+      str[107:111] + str[111:112] + str[0:6])

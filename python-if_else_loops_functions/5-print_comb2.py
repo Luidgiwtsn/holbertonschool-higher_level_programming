a92828bc3c6fb076ebed77152437c447a27a7890
luidgiwtsn <lwbordeaux@gmail.com>
2026-01-15 11:28:36 +0100

    Edit 5-print_comb2.py

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-This script prints numbers from 00 to 99, separated by ', '.
+Print numbers from 0 to 99 separated by ', '.
 Numbers are printed in ascending order with two digits.
-The last number is followed by a newline.
+Only one loop and no extra variables are used.
 """
 
 for i in range(100):
-    if i < 99:
-        print("%02d, " % i, end="")
+    if i != 99:
+        print("{:02d}, ".format(i), end="")
     else:
-        print("%02d" % i)
+        print("{:02d}".format(i))

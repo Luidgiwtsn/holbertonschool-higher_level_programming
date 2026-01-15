00b3bb4d29c592be04bd3910cf31326570388923
luidgiwtsn <lwbordeaux@gmail.com>
2026-01-15 10:54:25 +0100

    Edit pycodestyle 3-print_alphabet.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Print the ASCII alphabet in lowercase, excluding 'q' and 'e',
-without a trailing newline.converts an ASCII value to its 
+without a trailing newline.converts an ASCII value to its
 corresponding character.
 """
 

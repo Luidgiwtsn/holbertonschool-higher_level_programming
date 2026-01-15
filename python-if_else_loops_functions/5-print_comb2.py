7c43f2ea628d94c3cb5b297f5843985d8cb20d54
luidgiwtsn <lwbordeaux@gmail.com>
2026-01-15 11:19:46 +0100

    Edit 5-print_comb2.py

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -6,7 +6,4 @@ The last number is followed by a newline.
 """
 
 for i in range(100):
-    if i < 99:
-        print("{:02d}, ".format(i), end="")
-    else:
-        print("{:02d}".format(i))
+    print("{:02d}".format(i), end=", " if i < 99 else "\n")

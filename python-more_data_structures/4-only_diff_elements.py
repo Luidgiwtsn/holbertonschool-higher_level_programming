7085ce4dc962f6ca6fc1478002fb259680f6e747
luidgiwtsn <lwbordeaux@gmail.com>
2026-01-20 10:48:25 +0100

    Add 4-only_diff_elements.py|4-main.py

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,13 @@
+def only_diff_elements(set_1, set_2):
+    """Retourne un ensemble d'éléments présents dans
+    un seul des deux ensembles.
+
+    Args:
+        set_1: Premier ensemble
+        set_2: Deuxième ensemble
+
+    Returns:
+        Un ensemble contenant les éléments présents dans set_1 ou set_2,
+        mais pas dans les deux
+    """
+    return set_1 ^ set_2

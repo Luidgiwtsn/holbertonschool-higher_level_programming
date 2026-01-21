152fcaa0cb5ddf25b3d21b80649f414f6c376b7f
luidgiwtsn <lwbordeaux@gmail.com>
2026-01-21 09:34:48 +0100

    Edit pycodestyle 11-multiply_list_map.py

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python3
-"""Module that multiplies all values in a list by a number using map."""
-
-
+#!/usr/bin/python
 def multiply_list_map(my_list=[], number=0):
     return list(map(lambda x: x * number, my_list))

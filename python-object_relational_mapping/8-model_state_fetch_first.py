63d11e4e98f251ba987a8653d2f8d5f6f49557f5
luidgiwtsn <lwbordeaux@gmail.com>
2026-03-06 17:37:24 +0100

    Add script that print the first object from the table SQLalchemy

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""script that lists all State objects from the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    
+
+    Session = sessionmaker(bind=engine) # crée une fabrique de sessions
+    session = Session() # ouvre la session (comme un curseur)
+
+    state = session.query(State).order_by(State.id).first() # SELECT * FROM states ORDER BY id ASC .first() → retourne uniquement le premier objet 
+    print("{}: {}".format(state.id, state.name))
+
+    session.close()

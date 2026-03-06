0ca16a84cf77935923b3a21a00d13572416071ec
luidgiwtsn <lwbordeaux@gmail.com>
2026-03-06 11:49:24 +0100

    Edit shebang

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 Script that lists all states from the database hbtn_0e_0_usa
 Usage: ./0-select_states.py <mysql_username> <mysql_password> <database_name>
